961d18580afcf2753ff88e554a4f180536070ecc
Matthias Balke <matthias.balke@googlemail.com>
2025-04-28 11:16:39 +0200

    modified build scripts to work with HA configuration

--- collabora-code/scripts/start-collabora-online.sh
+++ collabora-code/scripts/start-collabora-online.sh
@@ -1,12 +1,48 @@
-#!/bin/sh
+#!/usr/bin/env bashio
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+bashio::log.info "Starting Collabora CODE Edition ..."
+
+# get HA addon config
+if bashio::config.has_value 'username'; then
+    USERNAME=$(bashio::config 'username')
+else
+    bashio::log.error "\"username\" not set. Please set \"username\" in add-on config. Aborting."
+    exit 1;
+fi
+if bashio::config.has_value 'password'; then
+    PASSWORD=$(bashio::config 'password')
+else
+    bashio::log.error "\"password\" not set. Please set \"password\" in add-on config. Aborting."
+    exit 1;
+fi
+if bashio::config.has_value 'server_name'; then
+    SERVER_NAME=$(bashio::config 'server_name')
+else
+    SERVER_NAME=""
+fi
+if bashio::config.true 'dont_gen_ssl_cert'; then
+    DONT_GEN_SSL_CERT=true
+else
+    unset DONT_GEN_SSL_CERT
+fi
+if bashio::config.has_value 'extra_params'; then
+    extra_params=$(bashio::config 'extra_params')
+else
+    extra_params=""
+fi
+
+if bashio::var.has_value "${SERVER_NAME}"; then
+    bashio::log.info "setting server name to: \"${SERVER_NAME}\""
+fi
+
 if test "${DONT_GEN_SSL_CERT-set}" = set; then
+bashio::log.info "Generating new self-signed certificates..."
 # Generate new SSL certificate instead of using the default
 mkdir -p /tmp/ssl/
-cd /tmp/ssl/
+cd /tmp/ssl/ || exit
 mkdir -p certs/ca
 openssl genrsa -out certs/ca/root.key.pem 2048
 openssl req -x509 -new -nodes -key certs/ca/root.key.pem -days 9131 -out certs/ca/root.crt.pem -subj "/C=DE/ST=BW/L=Stuttgart/O=Dummy Authority/CN=Dummy Authority"
@@ -26,5 +62,11 @@ cert_params="\
  --o:ssl.ca_file_path=/tmp/ssl/certs/ca/root.crt.pem"
 fi
 
+# store HA configured username and password (salted)
+bashio::log.info "Storing coolwsd username \"${USERNAME}\" and password..."
+coolconfig set-admin-password --user "${USERNAME}" --password "${PASSWORD}"
+bashio::log.info "done."
+
 # Start coolwsd
-exec /usr/bin/coolwsd --version --use-env-vars ${cert_params} --o:sys_template_path=/opt/cool/systemplate --o:child_root_path=/opt/cool/child-roots --o:file_server_root_path=/usr/share/coolwsd --o:cache_files.path=/opt/cool/cache --o:logging.color=false --o:stop_on_config_change=true ${extra_params} "$@"
+bashio::log.info "Starting coolwsd..."
+exec /usr/bin/coolwsd --version --use-env-vars "${cert_params:-}" --o:sys_template_path=/opt/cool/systemplate --o:child_root_path=/opt/cool/child-roots --o:file_server_root_path=/usr/share/coolwsd --o:cache_files.path=/opt/cool/cache --o:logging.color=false --o:stop_on_config_change=true "${extra_params:-}" "$@"
