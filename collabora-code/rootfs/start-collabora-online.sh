a56d81ec19b535175ddb02ef33092bb47026570e
Matthias Balke <Matthias.Balke@googlemail.com>
2025-05-13 01:08:31 +0200

    variable cleanup

--- collabora-code/rootfs/start-collabora-online.sh
+++ collabora-code/rootfs/start-collabora-online.sh
@@ -21,15 +21,18 @@ if bashio::config.exists 'dont_gen_ssl_cert' || bashio::config.exists 'extra_par
     bashio::log.info ""
 fi
 
+COOLWSD_CONFIG_FILE="/config/coolwsd.xml"
+COOLWSD_PROOF_KEY_CONFIG_FILE="/config/proof_key"
+
 # ensure config directory is readable by process user
 # this is needed, as after editing coolwsd.xml using smb, the owner is set to root
 chown -R cool:cool /config
 
 # expose coolwsd.xml in addon config directory
-if ! bashio::fs.file_exists "/config/coolwsd.xml"; then
-    cp -a /etc/coolwsd/coolwsd.xml /config/coolwsd.xml \
+if ! bashio::fs.file_exists "${COOLWSD_CONFIG_FILE}"; then
+    cp -a /etc/coolwsd/coolwsd.xml ${COOLWSD_CONFIG_FILE} \
         || bashio::exit.nok \
-            'Failed creating "/config/coolwsd.xml"'
+            "Failed creating \"${COOLWSD_CONFIG_FILE}\""
 fi
 
 # ensure config is readable by process user
@@ -37,16 +40,16 @@ fi
 chown -R cool:cool /config
 
 # generate WOPI proof key
-if ! bashio::fs.file_exists "/config/proof_key"; then
-    bashio::log.info "Generating WOPI proof key (/config/proof_key) ..."
-    sudo -H -u cool bash -c 'ssh-keygen -t rsa -N "" -m PEM -f /config/proof_key' \
+if ! bashio::fs.file_exists "${COOLWSD_PROOF_KEY_CONFIG_FILE}"; then
+    bashio::log.info "Generating WOPI proof key (${COOLWSD_PROOF_KEY_CONFIG_FILE}) ..."
+    sudo -H -u cool bash -c "ssh-keygen -t rsa -N \"\" -m PEM -f ${COOLWSD_PROOF_KEY_CONFIG_FILE}" \
         || bashio::exit.nok \
-            'Failed creating WOPI proof key "/config/proof_key"'
+            "Failed creating WOPI proof key \"${COOLWSD_PROOF_KEY_CONFIG_FILE}\""
     # until changing the configuration dir is fixed
     # symlink proof_key into default config directory
-    sudo -H -u cool bash -c 'ln -s /config/proof_key /etc/coolwsd/proof_key' \
+    sudo -H -u cool bash -c "ln -s ${COOLWSD_PROOF_KEY_CONFIG_FILE} /etc/coolwsd/proof_key" \
         || bashio::exit.nok \
-            'Failed creating symlink for proof key "/etc/coolwsd/proof_key -> /config/proof_key"'
+            "Failed creating symlink for proof key \"/etc/coolwsd/proof_key -> ${COOLWSD_PROOF_KEY_CONFIG_FILE}\""
 fi
 
 bashio::log.info "Starting Collabora CODE Edition ..."
@@ -114,11 +117,11 @@ fi
 
 # store HA configured username and password (salted)
 bashio::log.info "Storing coolwsd username \"${USERNAME}\" and password..."
-sudo -H -u cool bash -c "coolconfig --config-file /config/coolwsd.xml set-admin-password --user '${USERNAME}' --password '${PASSWORD}'"
+sudo -H -u cool bash -c "coolconfig --config-file ${COOLWSD_CONFIG_FILE} set-admin-password --user '${USERNAME}' --password '${PASSWORD}'"
 bashio::log.info "done."
 
 # Start coolwsd
 bashio::log.info "Starting coolwsd..."
 # explicitly allow spaces to separate arguments
 # shellcheck disable=SC2086
-sudo -H -u cool bash -c "exec /usr/bin/coolwsd --version --use-env-vars --config-dir /config --config-file /config/coolwsd.xml ${cert_params:-} --o:sys_template_path=/opt/cool/systemplate --o:child_root_path=/opt/cool/child-roots --o:file_server_root_path=/usr/share/coolwsd --o:cache_files.path=/opt/cool/cache --o:stop_on_config_change=true ${extra_params:-}"
+sudo -H -u cool bash -c "exec /usr/bin/coolwsd --version --use-env-vars --config-dir /config --config-file ${COOLWSD_CONFIG_FILE} ${cert_params:-} --o:sys_template_path=/opt/cool/systemplate --o:child_root_path=/opt/cool/child-roots --o:file_server_root_path=/usr/share/coolwsd --o:cache_files.path=/opt/cool/cache --o:stop_on_config_change=true ${extra_params:-}"
